d4e22b8d9cd09d8a7a1299da52d96c84dae21dd5
Sanara Felicio <sanara@cpqd.com.br>
2022-12-06 13:36:21 -0300

    lançando exceções com testes em junit

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,7 +1,7 @@
 package br.com.alura.tdd.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -16,12 +16,20 @@ class BonusServiceTest {
 	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
 
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Sanara", LocalDate.now(), new BigDecimal("25000.00")));
 
-		//verificando se o valor somado é igual a zero
-		assertEquals(new BigDecimal("0.00"), bonus);
+		// verificando se o método está lançando uma exceção
+//		assertThrows(IllegalArgumentException.class,
+//				() -> service.calcularBonus(new Funcionario("Sanara", LocalDate.now(), new BigDecimal("25000.00"))));
+		
+		try {
+			service.calcularBonus(new Funcionario("Sanara", LocalDate.now(), new BigDecimal("25000.00")));
+			//fail() método do jUnit para forçar um método a falhar
+			fail("Exceção não ocorreu");
+		} catch (Exception e) {			
+			assertEquals("Funcionário com salário maior do que R$10000 nao pode receber bonus!", e.getMessage());
+		}
 	}
-	
+
 	@Test
 	void bonusDeveriaSerDezPorCentoDoSalario() {
 
@@ -31,12 +39,12 @@ class BonusServiceTest {
 		assertEquals(new BigDecimal("250.00"), bonus);
 	}
 
-	
 	@Test
 	void bonusDeveriaSerDezPorCentoParaSalarioDeExatamenteDezMilReais() {
 
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Sanara", LocalDate.now(), new BigDecimal("10000.00")));
+		BigDecimal bonus = service
+				.calcularBonus(new Funcionario("Sanara", LocalDate.now(), new BigDecimal("10000.00")));
 
 		assertEquals(new BigDecimal("1000.00"), bonus);
 	}
